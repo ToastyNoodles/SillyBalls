879cf668f4dcc62fd3d2c2ffb044b77b0f9fa6d5
Anthony T <atrot1@gmail.com>
2024-05-22 13:59:36 -0700

    Silly practice mode that spawns spheres with physics above the main player.

--- CustomPlugin/CustomPluginComponent.cs
+++ CustomPlugin/CustomPluginComponent.cs
@@ -2,28 +2,38 @@
 using UnityEngine;
 using Comfort.Common;
 using BepInEx;
+using EFT.UI;
 
 namespace CustomPlugin
 {
     public class CustomPluginComponent : MonoBehaviour
     {
+        GameWorld gameWorld;
+
+        void Start()
+        {
+            gameWorld = Singleton<GameWorld>.Instance;
+        }
+
         void Update()
         {
-            if (UnityInput.Current.GetKeyDown(KeyCode.I))
+            if (gameWorld == null)
             {
-                SpawnPhysicsObject();
+                ConsoleScreen.Log("Custom Plugin failed to get GameWorld and will try again.");
+                gameWorld = Singleton<GameWorld>.Instance;
             }
+
+            SpawnPhysicsObject();
         }
 
         private void SpawnPhysicsObject()
         {
-            GameWorld gameWorld = Singleton<GameWorld>.Instance;
             if (gameWorld != null)
             {
                 GameObject physicsObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 physicsObject.AddComponent<Rigidbody>();
                 physicsObject.transform.position = gameWorld.MainPlayer.Transform.position + (Vector3.up * 2.0f);
-                float objectScale = Random.Range(0.1f, 3.0f);
+                float objectScale = Random.Range(0.1f, 1.0f);
                 physicsObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
                 physicsObject.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
             }
