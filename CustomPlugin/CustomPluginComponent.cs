45e9ea7217d585126790197f68d07a03d4be91a7
Anthony T <atrot1@gmail.com>
2024-05-22 14:19:21 -0700

    Looking into removing collision against player and added toggle for spawning physics objects with "I"

--- CustomPlugin/CustomPluginComponent.cs
+++ CustomPlugin/CustomPluginComponent.cs
@@ -8,7 +8,8 @@ namespace CustomPlugin
 {
     public class CustomPluginComponent : MonoBehaviour
     {
-        GameWorld gameWorld;
+        private GameWorld gameWorld;
+        private bool toggleSpawnPhysicsObject = false;
 
         void Start()
         {
@@ -24,7 +25,11 @@ namespace CustomPlugin
                 return;
             }
 
-            SpawnPhysicsObject();
+            if (UnityInput.Current.GetKeyDown(KeyCode.I))
+                toggleSpawnPhysicsObject = !toggleSpawnPhysicsObject;
+
+            if (toggleSpawnPhysicsObject)
+                SpawnPhysicsObject();
         }
 
         private void SpawnPhysicsObject()
@@ -33,10 +38,12 @@ namespace CustomPlugin
             {
                 GameObject physicsObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 physicsObject.AddComponent<Rigidbody>();
-                physicsObject.transform.position = gameWorld.MainPlayer.Transform.position + (Vector3.up * 2.0f);
+                physicsObject.transform.position = gameWorld.MainPlayer.Transform.position + (Vector3.up * 5.0f);
                 float objectScale = Random.Range(0.1f, 1.0f);
                 physicsObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
                 physicsObject.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+                physicsObject.GetComponent<Collider>().material.bounciness = 5.0f;
+                physicsObject.GetComponent<Rigidbody>().mass = 0.1f;
             }
         }
     }
